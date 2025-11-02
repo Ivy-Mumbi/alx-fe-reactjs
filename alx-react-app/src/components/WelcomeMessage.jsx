14d53c5051ca3ec9da0a32c5dcf609a3e2c291cd
Ivy-Mumbi <ivy.mumbi.5@gmail.com>
2025-11-02 22:16:55 +0300

    Add WelcomeMessage component and integrate into App

---
+++ alx-react-app/src/components/WelcomeMessage.jsx
@@ -0,0 +1,13 @@
+function WelcomeMessage() {
+    return (
+        <div>
+            {/* This heading is changed as requested */}
+            <h1>Hello everyone, I am learning React at ALX!</h1>
+            
+            {/* This is the new paragraph you were asked to add */}
+            <p>I am learning about JSX!</p>
+        </div>
+    );
+}
+
+export default WelcomeMessage;
